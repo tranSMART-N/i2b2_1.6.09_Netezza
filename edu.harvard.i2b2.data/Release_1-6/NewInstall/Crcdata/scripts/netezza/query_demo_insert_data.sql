233fa2eefe0da7f3d9edd4d122c204a910a8835a
harsha <bsharshabsharsha@gmail.com>
2013-10-28 20:45:16 +0530

    Removed the Database schema name form the file.

--- edu.harvard.i2b2.data/Release_1-6/NewInstall/Crcdata/scripts/netezza/query_demo_insert_data.sql
+++ edu.harvard.i2b2.data/Release_1-6/NewInstall/Crcdata/scripts/netezza/query_demo_insert_data.sql
@@ -1,9 +1,9 @@
-delete from I2B2DEMODATA.QT_BREAKDOWN_PATH;
+delete from QT_BREAKDOWN_PATH;
 
-insert into I2B2DEMODATA.QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_GENDER_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Gender\',current_date);
+insert into QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_GENDER_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Gender\',current_date);
 
-insert into I2B2DEMODATA.QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_RACE_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Race\',current_date);
+insert into QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_RACE_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Race\',current_date);
 
-insert into I2B2DEMODATA.QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_VITALSTATUS_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Vital Status\',current_date);
+insert into QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_VITALSTATUS_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Vital Status\',current_date);
 
-insert into I2B2DEMODATA.QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_AGE_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Age\',current_date);
+insert into QT_BREAKDOWN_PATH(NAME,VALUE,CREATE_DATE) values ('PATIENT_AGE_COUNT_XML','\\i2b2_DEMO\i2b2\Demographics\Age\',current_date);
